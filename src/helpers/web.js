4b25a181e6070c5d41b11cafc4575045e80c1d35
just-hope-netizen <hopepeter007@gmail.com>
2022-11-27 20:28:30 +0100

    chore: Changed backend API

--- src/helpers/web.js
+++ src/helpers/web.js
@@ -1,4 +1,4 @@
-const backendHost = 'https://ceevo-server.herokuapp.com/';
+const backendHost = 'https://login-system-backend-production.up.railway.app/';
 // const backendHost = 'http://localhost:2000/'
 
 export function register(data) {
